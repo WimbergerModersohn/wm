da96ac419423ee62d62126d99a51ed89124984b4
bd1st1r <fbdnstnr@icloud.com>
2022-11-15 16:35:14 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.html',
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: []
+}
